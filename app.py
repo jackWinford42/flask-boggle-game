4898af34bb5e014891608e6296767d661f22994f
JackWinford <winforj2@wwu.edu>
2021-04-07 17:14:40 -0700

    deleted problematic function

--- app.py
+++ app.py
@@ -7,14 +7,6 @@ app.config['SECRET_KEY'] = "secretKey"
 
 boggle_game = Boggle()
 
-
-def dir_last_updated(folder):
-    """This function allows the flask app and chrome browser work
-    so that my most recently saved file is used by the running server"""
-    return str(max(os.path.getmtime(os.path.join(root_path, f))
-                    for root_path, dirs, files in os.walk(folder)
-                    for f in files))
-
 @app.route('/')
 def start_page():
     """Display the home/start page of the survey app"""
@@ -25,7 +17,6 @@ def start_page():
     n_plays = session.get("n_plays", 0)
 
     return render_template("board.html",
-                            last_updated=dir_last_updated('/home/jw/Programs/SpringBoard/flask-boggle/static'),
                             board=game_board,
                             highscore=highscore,
                             n_plays=n_plays)
